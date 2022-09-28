f0340f1315e9e1ff61f1256d3c88b661cf28ab44
Kimonae <shinokasu@gmail.com>
2022-09-28 12:13:48 +0200

    version à jour

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './core/components/footer/footer.component';
-import { HeaderComponent } from './core/components/header/header.component';
-import { NavComponent } from './core/components/nav/nav.component';
 import { CoreModule } from './core/core.module';
 
 @NgModule({

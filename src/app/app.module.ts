d3af77e163fcfe11ea26c090b0b70575c9b674dd
Maryline-Bellanger <maryline.bellanger45@gmail.com>
2022-09-28 12:21:16 +0200

    update app-component.html and app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './core/components/footer/footer.component';
-import { HeaderComponent } from './core/components/header/header.component';
-import { NavComponent } from './core/components/nav/nav.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
-    HeaderComponent,
-    NavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
